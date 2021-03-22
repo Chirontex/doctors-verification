fa8219f0d1ab81f6c6e117ecbe99ac480e8702ca
Dmitry Shumilin <infernozzz@yandex.ru>
2021-03-22 12:10:39 +0300

    implement time and tries checking

--- src/Main.php
+++ src/Main.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Doctors Verification 0.1.5 by Dmitry Shumilin
+ * Doctors Verification 0.1.6 by Dmitry Shumilin
  * License: GNU GPL v3, see LICENSE
  */
 namespace Chirontex\DocsVer;
@@ -8,7 +8,6 @@ namespace Chirontex\DocsVer;
 use Chirontex\DocsVer\Providers\Status;
 use Chirontex\DocsVer\Providers\Testing;
 use Chirontex\DocsVer\Exceptions\MainException;
-use Chirontex\DocsVer\Exceptions\StatusException;
 use Chirontex\DocsVer\Exceptions\ExceptionsList;
 use CDatabase;
 
@@ -19,6 +18,8 @@ class Main
     protected $user_id;
     protected $status_provider;
     protected $status_data;
+    protected $testing_provider;
+    protected $testing_data;
 
     public function __construct(int $user_id, CDatabase $db)
     {
@@ -36,6 +37,22 @@ class Main
 
         $this->status_data = $this->status_provider->statusGet($this->user_id);
 
+        if (empty(
+            $this->status_data
+        )) $this->status_data = $this->status_provider
+        ->statusSet($this->user_id, 0)
+        ->statusGet($this->user_id);
+
+        $this->testing_provider = new Testing($this->db);
+
+        $this->testing_data = $this->testing_provider->dataGet($this->user_id);
+
+        $this->testing_data = empty($this->testing_data) ?
+            $this->testing_provider
+                ->dataCreate($this->user_id)
+                ->dataGet($this->user_id) :
+            $this->testing_data;
+
     }
 
     /**
@@ -51,22 +68,61 @@ class Main
 
     }
 
+    /**
+     * Check available testing tries and launch
+     * the testing or not.
+     * 
+     * @return $this
+     */
     public function testingInit() : self
     {
 
-        $testing = new Testing($this->db);
+        $day_passed = (time() - strtotime(
+            $this->status_data['modified']
+        )) >= 86400;
 
-        $data = $testing->dataGet($this->user_id);
+        if ($day_passed) {
+            
+            $this->testing_provider->dataUpdate($this->user_id, 3);
 
-        $data = empty($data) ?
-            $testing
-                ->dataCreate($this->user_id)
-                ->dataGet($this->user_id) :
-            $data;
+            $this->testing_data['tries'] = 3;
+        
+        }
 
-        $this->status_data = $this->status_provider
-            ->statusSet($this->user_id, 0)
-            ->statusGet($this->user_id);
+        if ((int)$this->testing_data['tries'] === 0 &&
+            !$day_passed) $this->testingDeny();
+        elseif ((int)$this->testing_data['tries'] > 0) {
+
+            $this->status_data = $this->status_provider
+                ->statusSet($this->user_id, 0)
+                ->statusGet($this->user_id);
+
+            $this->testing_provider->dataUpdate(
+                $this->user_id,
+                (int)$this->testing_data['tries'] - 1
+            );
+
+            $this->testing_data['tries'] -= 1;
+
+            $this->testingProcess();
+
+        }
+
+        return $this;
+
+    }
+
+    protected function testingDeny() : self
+    {
+
+        echo 'Вы исчерпали свои попытки пройти тест за эти сутки.';
+
+        return $this;
+
+    }
+
+    protected function testingProcess() : self
+    {
 
         echo 'Your status data:<br />';
 
@@ -75,7 +131,7 @@ class Main
         echo '<br />';
         echo 'Your testing data:<br />';
 
-        var_dump($data);
+        var_dump($this->testing_data);
 
         return $this;
 
